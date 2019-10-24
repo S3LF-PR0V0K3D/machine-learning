1db88f17f350384049df41868bcd4481d0d75446
S3LF-PR0V0K3D <56975419+S3LF-PR0V0K3D@users.noreply.github.com>
2019-10-24 14:12:38 -0400

    Add files via upload

---
+++ aprendizado_supervisionado.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 10:38:50 2019
+Machine learning aprendizado supervisionado
+@author: Felipe Soares
+"""
+
+
+import pandas as pd
+import time
+import seaborn as sns
+import matplotlib.pyplot as plt
+import datetime
+import numpy as np
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+
+bitcoin_market_info = pd.read_html("https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20130428&end="+time.strftime("%Y%m%d"))[0]
+bitcoin_market_info = bitcoin_market_info.assign(Date=pd.to_datetime(bitcoin_market_info['Date']))
+bitcoin_market_info.loc[bitcoin_market_info['Volume']=="-",'Volume']=0
+bitcoin_market_info['Volume'] = bitcoin_market_info['Volume'].astype('int64')
+bitcoin_market_info.columns = bitcoin_market_info.columns.str.replace("*", "")
+bitcoin_market_info.head()
+
+eth_market_info = pd.read_html("https://coinmarketcap.com/currencies/ethereum/historical-data/?start=20130428&end="+time.strftime("%Y%m%d"))[0]
+eth_market_info = eth_market_info.assign(Date=pd.to_datetime(eth_market_info['Date']))
+eth_market_info.columns = eth_market_info.columns.str.replace("*", "")
+eth_market_info.head()
+
+bitcoin_market_info.columns =[bitcoin_market_info.columns[0]]+['bt_'+i for i in bitcoin_market_info.columns[1:]]
+eth_market_info.columns =[eth_market_info.columns[0]]+['eth_'+i for i in eth_market_info.columns[1:]]
+
+market_info = pd.merge(bitcoin_market_info,eth_market_info, on=['Date'])
+market_info = market_info[market_info['Date']>='2018-01-01']
+for coins in ['bt_', 'eth_']: 
+    kwargs = { coins+'day_diff': lambda x: (x[coins+'Close']-x[coins+'Open'])/x[coins+'Open']}
+    market_info = market_info.assign(**kwargs)
+market_info.head()
+
+split_date = '2019-01-01'
+fig, (ax1, ax2) = plt.subplots(2,1)
+ax1.set_xticks([datetime.date(i,j,1) for i in range(2019,2019) for j in [1,7]])
+ax1.set_xticklabels('')
+ax2.set_xticks([datetime.date(i,j,1) for i in range(2019,2019) for j in [1,7]])
+ax2.set_xticklabels([datetime.date(i,j,1).strftime('%b %Y')  for i in range(2019,2019) for j in [1,7]])
+ax1.plot(market_info[market_info['Date'] < split_date]['Date'].astype('datetime64[ns]'),
+         market_info[market_info['Date'] < split_date]['bt_Close'], 
+         color='#B08FC7', label='Treinamento')
+ax1.plot(market_info[market_info['Date'] >= split_date]['Date'].astype('datetime64[ns]'),
+         market_info[market_info['Date'] >= split_date]['bt_Close'], 
+         color='#8FBAC8', label='Teste')
+ax2.plot(market_info[market_info['Date'] < split_date]['Date'].astype('datetime64[ns]'),
+         market_info[market_info['Date'] < split_date]['eth_Close'], 
+         color='#B08FC7')
+ax2.plot(market_info[market_info['Date'] >= split_date]['Date'].astype('datetime64[ns]'),
+         market_info[market_info['Date'] >= split_date]['eth_Close'], color='#8FBAC8')
+ax1.set_xticklabels('')
+ax1.set_ylabel('Bitcoin Preco ($)',fontsize=12)
+ax2.set_ylabel('Ethereum Preco ($)',fontsize=12)
+plt.tight_layout()
+ax1.legend(bbox_to_anchor=(0.03, 1), loc=2, borderaxespad=0., prop={'size': 14})
+plt.show()
+
+fig, (ax1, ax2) = plt.subplots(2,1)
+ax1.set_xticks([datetime.date(2017,i+1,1) for i in range(12)])
+ax1.set_xticklabels('')
+ax2.set_xticks([datetime.date(2017,i+1,1) for i in range(12)])
+ax2.set_xticklabels([datetime.date(2017,i+1,1).strftime('%b %d %Y')  for i in range(12)])
+ax1.plot(market_info[market_info['Date']>= split_date]['Date'].astype('datetime64[ns]'),
+         market_info[market_info['Date']>= split_date]['bt_Close'].values, label='Actual')
+ax1.plot(market_info[market_info['Date']>= split_date]['Date'].astype('datetime64[ns]'),
+          market_info[market_info['Date']>= datetime.datetime.strptime(split_date, '%Y-%m-%d') - 
+                      datetime.timedelta(days=1)]['bt_Close'][1:].values, label='Predicted')
+ax1.set_ylabel('Bitcoin Preco ($)',fontsize=12)
+ax1.legend(bbox_to_anchor=(0.1, 1), loc=2, borderaxespad=0., prop={'size': 14})
+ax1.set_title('Modelo de Lag Simples (Conjunto de Testes)')
+ax2.set_ylabel('Etherum Preco ($)',fontsize=12)
+ax2.plot(market_info[market_info['Date']>= split_date]['Date'].astype('datetime64[ns]'),
+         market_info[market_info['Date']>= split_date]['eth_Close'].values, label='Atual')
+ax2.plot(market_info[market_info['Date']>= split_date]['Date'].astype('datetime64[ns]'),
+          market_info[market_info['Date']>= datetime.datetime.strptime(split_date, '%Y-%m-%d') - 
+                      datetime.timedelta(days=1)]['eth_Close'][1:].values, label='Predicted')
+fig.tight_layout()
+plt.show()
+
+fig, (ax1, ax2) = plt.subplots(1,2)
+ax1.hist(market_info[market_info['Date']< split_date]['bt_day_diff'].values, bins=100)
+ax2.hist(market_info[market_info['Date']< split_date]['eth_day_diff'].values, bins=100)
+ax1.set_title('Mudanças no preço diário do Bitcoin')
+ax2.set_title('Mudanças Ethereum')
+plt.show()
+
+# Começando LSTM
+
+for coins in ['bt_']: 
+    kwargs = { coins+'close_off_high': lambda x: 2*(x[coins+'High']- x[coins+'Close'])/(x[coins+'High']-x[coins+'Low'])-1,
+            coins+'volatility': lambda x: (x[coins+'High']- x[coins+'Low'])/(x[coins+'Open'])}
+    market_info = market_info.assign(**kwargs)
+    
+model_data = market_info[['Date']+[coin+metric for coin in ['bt_'] 
+                                   for metric in ['Close','Volume','close_off_high','volatility']]]
+# need to reverse the data frame so that subsequent rows represent later timepoints
+model_data = model_data.sort_values(by='Date')
+model_data.head()
+
+# Retirar coluna date
+training_set, test_set = model_data[model_data['Date']<split_date], model_data[model_data['Date']>=split_date]
+training_set = training_set.drop('Date', 1)
+test_set = test_set.drop('Date', 1)
+
+window_len = 10
+norm_cols = [coin+metric for coin in ['bt_'] for metric in ['Close','Volume']] 
+
+LSTM_training_inputs = []
+
+for i in range(len(training_set)-window_len):
+    temp_set = training_set[i:(i+window_len)].copy()
+    for col in norm_cols:
+        temp_set.loc[:, col] = temp_set[col]/temp_set[col].iloc[0] - 1
+    LSTM_training_inputs.append(temp_set)
+LSTM_training_outputs = (training_set['bt_Close'][window_len:].values/training_set['bt_Close'][:-window_len].values)-1
+
+LSTM_test_inputs = []
+for i in range(len(test_set)-window_len):
+    temp_set = test_set[i:(i+window_len)].copy()
+    for col in norm_cols:
+        temp_set.loc[:, col] = temp_set[col]/temp_set[col].iloc[0] - 1
+    LSTM_test_inputs.append(temp_set)
+LSTM_test_outputs = (test_set['bt_Close'][window_len:].values/test_set['bt_Close'][:-window_len].values)-1
+LSTM_training_inputs[0]
+
+LSTM_training_inputs = [np.array(LSTM_training_input) for LSTM_training_input in LSTM_training_inputs]
+LSTM_training_inputs = np.array(LSTM_training_inputs)
+
+LSTM_test_inputs = [np.array(LSTM_test_inputs) for LSTM_test_inputs in LSTM_test_inputs]
+LSTM_test_inputs = np.array(LSTM_test_inputs)
+
+from keras.models import Sequential
+from keras.layers import Activation, Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+def build_model(inputs, output_size, neurons, activ_func="linear",
+                dropout=0.25, loss="mae", optimizer="adam"):
+    model = Sequential()
+
+    model.add(LSTM(neurons, input_shape=(inputs.shape[1], inputs.shape[2])))
+    model.add(Dropout(dropout))
+    model.add(Dense(units=output_size))
+    model.add(Activation(activ_func))
+
+    model.compile(loss=loss, optimizer=optimizer)
+    return model
+
+# semente aleatória para reprodutibilidade
+np.random.seed(202)
+# inicializar a arquitetura do modelo
+eth_model = build_model(LSTM_training_inputs, output_size=1, neurons = 20)
+# a saída do modelo é o próximo preço normalizado para o 10º preço de fechamento anterior
+LSTM_training_outputs = (training_set['bt_Close'][window_len:].values/training_set['bt_Close'][:-window_len].values)-1
+# Modelo de treinamento
+# note: eth_history contém informações sobre o erro de treinamento por época
+eth_history = eth_model.fit(LSTM_training_inputs, LSTM_training_outputs, 
+                            epochs=50, batch_size=1, verbose=2, shuffle=True)
+
+
+fig, ax1 = plt.subplots(1,1)
+
+ax1.plot(eth_history.epoch, eth_history.history['loss'])
+ax1.set_title('Training Error')
+
+if eth_model.loss == 'mae':
+    ax1.set_ylabel('Mean Absolute Error (MAE)',fontsize=12)
+# just in case you decided to change the model loss calculation
+else:
+    ax1.set_ylabel('Model Loss',fontsize=12)
+ax1.set_xlabel('# Epochs',fontsize=12)
+plt.show()
+
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+
+fig, ax1 = plt.subplots(1,1)
+ax1.set_xticks([datetime.date(i,j,1) for i in range(2017,2019) for j in [1,5,9]])
+ax1.set_xticklabels([datetime.date(i,j,1).strftime('%b %Y')  for i in range(2017,2019) for j in [1,5,9]])
+ax1.plot(model_data[model_data['Date']< split_date]['Date'][window_len:].astype('datetime64[ns]'),
+         training_set['bt_Close'][window_len:], label='Atual')
+ax1.plot(model_data[model_data['Date']< split_date]['Date'][window_len:].astype('datetime64[ns]'),
+         ((np.transpose(eth_model.predict(LSTM_training_inputs))+1) * training_set['bt_Close'].values[:-window_len])[0], 
+         label='Predicted')
+ax1.set_title('Conjunto de treinamento: Previsão de ponto único')
+ax1.set_ylabel('Bitcoin Preco ($)',fontsize=12)
+ax1.legend(bbox_to_anchor=(0.15, 1), loc=2, borderaxespad=0., prop={'size': 14})
+ax1.annotate('MAE: %.4f'%np.mean(np.abs((np.transpose(eth_model.predict(LSTM_training_inputs))+1)-\
+            (training_set['bt_Close'].values[window_len:])/(training_set['bt_Close'].values[:-window_len]))), 
+             xy=(0.75, 0.9),  xycoords='axes fraction',
+            xytext=(0.75, 0.9), textcoords='axes fraction')
+# figure inset code taken from http://akuederle.com/matplotlib-zoomed-up-inset
+axins = zoomed_inset_axes(ax1, 3.35, loc=10) # zoom-factor: 3.35, location: centre
+axins.set_xticks([datetime.date(i,j,1) for i in range(2017,2019) for j in [1,5,9]])
+axins.plot(model_data[model_data['Date']< split_date]['Date'][window_len:].astype('datetime64[ns]'),
+         training_set['bt_Close'][window_len:], label='Actual')
+axins.plot(model_data[model_data['Date']< split_date]['Date'][window_len:].astype('datetime64[ns]'),
+         ((np.transpose(eth_model.predict(LSTM_training_inputs))+1) * training_set['bt_Close'].values[:-window_len])[0], 
+         label='Predicted')
+axins.set_xlim([datetime.date(2017, 3, 1), datetime.date(2019, 4, 1)])
+axins.set_ylim([10,60])
+axins.set_xticklabels('')
+mark_inset(ax1, axins, loc1=1, loc2=3, fc="none", ec="0.5")
+plt.figure(figsize=(10,7))
+plt.show()
